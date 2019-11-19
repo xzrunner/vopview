b3356d74f086fd6a15e1df0bbdd5481005704b72
zz <xzrunner@gmail.com>
2019-11-20 07:45:21 +0800

    [CHANGED] up vop, use hdiop

--- source/Node.cpp
+++ source/Node.cpp
@@ -65,17 +65,17 @@ void Node::InitPins(const std::string& name)
 	assert(method_imports.is_valid());
 	auto var_imports = method_imports.invoke(var);
 	assert(var_imports.is_valid()
-		&& var_imports.is_type<std::vector<vop::Node::Port>>());
-	auto& imports = var_imports.get_value<std::vector<vop::Node::Port>>();
+		&& var_imports.is_type<std::vector<hdiop::NodePort<vop::VariableType>>>());
+	auto& imports = var_imports.get_value<std::vector<hdiop::NodePort<vop::VariableType>>>();
 
 	auto method_exports = t.get_method("GetExports");
 	assert(method_exports.is_valid());
 	auto var_exports = method_exports.invoke(var);
 	assert(var_exports.is_valid()
-		&& var_exports.is_type<std::vector<vop::Node::Port>>());
-	auto& exports = var_exports.get_value<std::vector<vop::Node::Port>>();
+		&& var_exports.is_type<std::vector<hdiop::NodePort<vop::VariableType>>>());
+	auto& exports = var_exports.get_value<std::vector<hdiop::NodePort<vop::VariableType>>>();
 
-	auto rg2grp = [](std::vector<PinDesc>& dst, const std::vector<vop::Node::Port>& src)
+	auto rg2grp = [](std::vector<PinDesc>& dst, const std::vector<hdiop::NodePort<vop::VariableType>>& src)
 	{
 		dst.reserve(dst.size() + src.size());
 		for (int i = 0, n = src.size(); i < n; ++i)
